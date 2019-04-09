51dcf218a4592ff71bf4e7ab5a62efc78ce9f402
caigengtao <18826076160@163.comm>
2019-04-10 01:43:13 +0800

    增加C语言编译器

---
+++ c语言编译器(python版)/procedure.c
@@ -0,0 +1,24 @@
+void  main()
+{   
+    ;
+    int a;
+    int b;
+    ;
+    ;
+    ;
+    a=1;
+    b=3;
+    while(a<b){
+        ;
+        ;
+        a=3;
+        scanf("%d",&a);
+        printf("%d",a);
+    }
+    if(a<b){
+        ;
+        a=2;
+        printf("%d",a);
+    }
+    return 0;
+}
